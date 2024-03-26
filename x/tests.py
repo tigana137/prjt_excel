43ea369498ebbb3ca5178250ce563d7e89197836
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2024-03-26 11:01:27 +0200

    lebes

--- x/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 05b98efc67f75b0e94f2311b07801f92443b2d3a
